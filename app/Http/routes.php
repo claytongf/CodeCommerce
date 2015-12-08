3e43ae5eae8b8a3ed5393b2d40ce67c6b83b05e0
Clayton Figueiredo <clayton@techwing.com.br>
2015-12-08 10:52:20 -0200

    Adicionado operações de criação, listagem, deleção e edição de categorias e produtos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,14 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('admin/category', 'AdminCategoriesController@index');
-//
-//Route::get('admin/products', 'AdminProductsController@index');
-
 Route::group(['prefix' => 'admin'], function(){
     Route::get('category', 'AdminCategoriesController@index');
     Route::get('products', 'AdminProductsController@index');
 });
+
+Route::get('categories', ['as'=>'categories', 'uses'=>'CategoriesController@index']);
+Route::post('categories', ['as'=>'categories.store', 'uses'=>'CategoriesController@store']);
+Route::get('categories/create', ['as'=>'categories.create', 'uses'=>'CategoriesController@create']);
+Route::get('categories/{id}/edit', ['as'=>'categories.edit', 'uses'=>'CategoriesController@edit']);
+Route::put('categories/{id}/update', ['as'=>'categories.update', 'uses'=>'CategoriesController@update']);
+Route::get('categories/{id}/destroy', ['as'=>'categories.destroy', 'uses'=>'CategoriesController@destroy']);
+
+Route::get('products', ['as'=>'products', 'uses'=>'ProductsController@index']);
+Route::post('products', ['as'=>'products.store', 'uses'=>'ProductsController@store']);
+Route::get('products/create', ['as'=>'products.create', 'uses'=>'ProductsController@create']);
+Route::get('products/{id}/edit', ['as'=>'products.edit', 'uses'=>'ProductsController@edit']);
+Route::put('products/{id}/update', ['as'=>'products.update', 'uses'=>'ProductsController@update']);
+Route::get('products/{id}/destroy', ['as'=>'products.destroy', 'uses'=>'ProductsController@destroy']);
 
 //Route::get('admin/category', function () {
 //    $categories = CodeCommerce\Category::all();
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
 
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -22,7 +22,7 @@
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -31,22 +31,18 @@
 				<a class="navbar-brand" href="#">Laravel</a>
 			</div>
 
-			<div class="collapse navbar-collapse" id="navbar">
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Welcome</a></li>
+					<li><a href="{{ url('/') }}">Home</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
-					@if(auth()->guest())
-						@if(!Request::is('auth/login'))
-							<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						@endif
-						@if(!Request::is('auth/register'))
-							<li><a href="{{ url('/auth/register') }}">Register</a></li>
-						@endif
+					@if (Auth::guest())
+						<li><a href="{{ url('/auth/login') }}">Login</a></li>
+						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
@@ -61,6 +57,6 @@
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
