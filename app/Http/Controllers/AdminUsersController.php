efc6d064e6532edaae33292ffadd7ab68428037a
Clayton Figueiredo <clayton@techwing.com.br>
2016-08-02 18:07:18 -0300

    Adicionado dados Fakes no banco. Criado gerenciamento de usuários

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\Product;
+
+class AdminProductsController extends Controller
+{
+    private $productModel;
+    
+    function __construct(Product $productModel) {
+        $this->productModel = $productModel;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = $this->productModel->all();
+        return view('admin/products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin/products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Requests\ProductRequest $request)
+    {
+        $input = $request->all();
+        $product = $this->productModel->fill($input);
+        $product->save();
+        return redirect()->route('admin/products');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = $this->productModel->find($id);
+        return view('admin/products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Requests\ProductRequest $request, $id)
+    {
+        $this->productModel->find($id)->update($request->all());
+        return redirect()->route('admin/products');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->productModel->find($id)->delete();
+        return redirect()->route('admin/products');
+    }
+}
